82796db80c156264f714672a8ce7906b4b57d076
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-04-05 09:26:28 +0500

    Lab

---
+++ assignment/PFLAB_02/task1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    /**
+     * ?cout
+     *
+     * Is use to print anything you want i.e var, string etc to console
+     */
+
+    /**
+     * !\n
+     *
+     * Is use for break line
+     */
+
+    cout << "1. Wakeup \n";
+    cout << "2. Get on the bed and wear the shoes \n";
+    cout << "3. Open the cupboard and choose the dress according to your choice. \n";
+    cout << "4. Close the cupboard \n";
+    cout << "5. Head towards bath \n";
+    cout << "6. Brush your tooth or take bath or wash your face and hands \n";
+    cout << "7. Now dress up and exit from bath \n";
+    cout << "8. Wear the shoes and pick your belongings \n";
+    cout << "9. Checkout from your room and head towards your ride \n";
+    cout << "10. Start the ride and start following your university path \n";
+    cout << "11. After reaching university, park your ride and head towards university gate \n";
+    cout << "12. Scan your card at university gate to enter the  university \n";
+
+    system("pause");
+
+    return 0;
+}
