e0290573d8b495bb28173372c40a572058786f2f
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-04-05 09:39:02 +0500

    La

---
+++ assignment/PFLAB_02/task2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    /**
+     * ?cout
+     *
+     * Is used to print anything you want i.e var, string etc to console
+     */
+
+    /**
+     * !\n
+     *
+     * Is used for break line
+     */
+
+    cout << "1. Create variable of 'int' type \n";
+    cout << "2. Take input using 'cin' (namespace must be included at top, else use static 'std' class method) \n";
+    cout << "3. Now you can create a separate function or you can check in 'if-else' whether the remainder of input after dividing by 13 is 0 or not \n";
+    cout << "4. If remainder is 0, you can print 'Entered number is multiple of 13' else you can print 'Entered number is not multiple of 13' \n";
+    cout << "5. At last, you can return 0 if 'return type' of function is int, else you can run code now. \n";
+
+    system("pause");
+
+    return 0;
+}
